6a494f66ece3d50cdadbbeee7a6600675f1f8166
sandrosaric <sandro.saric@gmail.com>
2022-11-15 16:30:08 +0100

    Migrated to multi-module microservice project

---
+++ shopping-microservices-system/inventory-service/src/main/java/com/sandrosaric/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,12 @@
+package com.sandrosaric.inventoryservice.repository;
+
+import com.sandrosaric.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+     Optional<Inventory> findBySkuCode(String skuCode);
+}
