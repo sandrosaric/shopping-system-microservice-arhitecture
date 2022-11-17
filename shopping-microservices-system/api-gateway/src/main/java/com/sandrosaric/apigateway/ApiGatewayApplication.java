8a61296c1837d0b6d03889a4a36e759217df1ef9
sandrosaric <sandro.saric@gmail.com>
2022-11-17 13:55:58 +0100

    Implemented Service Discovery and API Gateway pattern

--- shopping-microservices-system/api-gateway/src/main/java/com/sandrosaric/apigateway/ApiGatewayApplication.java
+++ shopping-microservices-system/api-gateway/src/main/java/com/sandrosaric/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.sandrosaric.orderservice;
+package com.sandrosaric.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class OrderServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
--- shopping-microservices-system/product-service/src/main/java/com/sandrosaric/productservice/ProductServiceApplication.java
+++ shopping-microservices-system/product-service/src/main/java/com/sandrosaric/productservice/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.sandrosaric.productservice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 
